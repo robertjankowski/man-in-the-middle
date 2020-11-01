cfb9c5a49a863c46a3263f7447ba657cb868f33d
robertjankowski <robjanw11@wp.pl>
2020-11-01 12:56:13 +0100

    add proxychaining

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 ## Repo for scripts and notes for ADT project
+
+1. Proxy chaining
+
+    
+  ```
+  sudo apt-get install proxychains
+  ```
+ 
+  - copy `proxychains.conf` into `/etc/proxychains.conf`
+
+  - when running `nmap` use proxy chaining as follows:
+    
+  ```
+  sudo proxychains nmap -sT SOME_IP_ADDRESS
+  ```
+
+2. Port scanning
+  
+  
+
+
--- find_open_ports.sh
+++ find_open_ports.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 
-ip=$1
+usage() {
+  echo "Port scanning script"
+  echo "Usage: ./find_open_ports.sh SOME_IP_ADDR 0"
+  echo "SOME_IP_ADDR -- ip of victim"
+  echo "0 - run without proxychains, 1 - run with proxychains"
+}
 
-ports=$(nmap -p- --min-rate=1000 -T4 $ip | grep ^[0-9] | cut -d '/' -f 1 | tr '\n' ',' | sed s/,$//)
+if [ "$#" -ne 2 ]; then
+  usage
+  exit
+fi
+
+ip=$1
+proxychains=$2
+
+ports_command="nmap -p- --min-rate=1000 -T4 $ip | grep ^[0-9] | cut -d '/' -f
+1 | tr '\n' ',' | sed s/,$//"
+
+if [ $proxychains == 1 ]; then
+  ports_command="proxychains "$ports_command
+fi
+
+ports=$(eval $ports_command)
+
+if [ "$ports" == "" ]; then
+  echo "No open ports available on $ip"
+  exit
+fi
+
+echo "Scanning ports..."$ports
 nmap -sC -sV -p$ports $ip
